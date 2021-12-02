5010b20e68cb05e1db706bdb778b5a0f017b5ecb
basbuijsen <bbuijsen@gmail.com>
2021-12-02 15:30:31 +0100

    simplified code

--- day1.java
+++ day1.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class day1 {
@@ -9,54 +10,33 @@ public class day1 {
     }
 
     public static int exerciseOne() throws FileNotFoundException {
-        File inputFile = new File("input.txt");
-        Scanner s = new Scanner(inputFile);
-
+        Scanner s = new Scanner(new File("input.txt"));
         int counter = 0;
-
-        String current = "";
-        String previous = "";
-
+        String current = "", previous = "";
         while (s.hasNext()) {
             previous = current;
             current = s.next();
-
-            if (current != "" && previous != "") {
-                if (Integer.valueOf(previous) < Integer.valueOf(current)) {
+            if (current != "" && previous != "")
+                if (Integer.valueOf(previous) < Integer.valueOf(current))
                     counter++;
-                }
-            }
         }
-
         return counter;
     }
 
     public static int exerciseTwo() throws FileNotFoundException {
-        File inputFile = new File("input.txt");
-        Scanner s = new Scanner(inputFile);
-
+        Scanner s = new Scanner(new File("input.txt"));
         int counter = 0;
-
-        String current = "";
-        String previous = "";
-        String preprevious = "";
-        String oldest = "";
-
+        LinkedList<String> list = new LinkedList<>();
         while (s.hasNext()) {
-            oldest = preprevious;
-            preprevious = previous;
-            previous = current;
-            current = s.next();
-
-            if (current != "" && previous != "" && preprevious != "" && oldest != "") {
-                int sum1 = Integer.valueOf(current) + Integer.valueOf(previous) + Integer.valueOf(preprevious);
-                int sum2 = Integer.valueOf(previous) + Integer.valueOf(preprevious) + Integer.valueOf(oldest);
-                if (sum2 < sum1) {
+            list.add(s.next());
+            if (list.size() > 4) {
+                list.removeFirst();
+                if ((Integer.valueOf(list.get(2)) + Integer.valueOf(list.get(1))
+                        + Integer.valueOf(list.get(0))) < (Integer.valueOf(list.get(3)) + Integer.valueOf(list.get(2))
+                                + Integer.valueOf(list.get(1))))
                     counter++;
-                }
             }
         }
-
         return counter;
     }
 }

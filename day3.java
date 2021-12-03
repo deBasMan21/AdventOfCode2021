69bfe6ba1257e06272939bbb3ace7b02bb95988c
Bas <bbuijsen@gmail.com>
2021-12-03 16:17:48 +0100

    simplified code

--- day3.java
+++ day3.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class day3 {
@@ -14,29 +15,21 @@ public class day3 {
         Scanner s = new Scanner(new File("input.txt"));
         LinkedList<HashMap<Integer, Integer>> bits = new LinkedList<>();
         String highest = "", lowest = "";
-        for (int i = 0; i < 12; i++) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-            map.put(1, 0);
-            map.put(0, 0);
-            bits.add(map);
-        }
+        for (int i = 0; i < 12; i++)
+            bits.add(new HashMap<Integer, Integer>(Map.of(1, 0, 0, 0)));
         while (s.hasNext()) {
             String[] input = s.next().split("");
-            for (int i = 0; i < input.length; i++) {
-                int amount = bits.get(i).get(Integer.valueOf(input[i])) + 1;
-                bits.get(i).put(Integer.valueOf(input[i]), amount);
-            }
+            for (int i = 0; i < input.length; i++)
+                bits.get(i).put(Integer.valueOf(input[i]), bits.get(i).get(Integer.valueOf(input[i])) + 1);
         }
-        for (int i = 0; i < 12; i++) {
-            int one = bits.get(i).get(0), two = bits.get(i).get(1);
-            if (Math.max(Integer.valueOf(one), Integer.valueOf(two)) == Integer.valueOf(one)) {
+        for (int i = 0; i < 12; i++)
+            if (Math.max(bits.get(i).get(0), bits.get(i).get(1)) == bits.get(i).get(0)) {
                 highest += "1";
                 lowest += "0";
             } else {
                 highest += "0";
                 lowest += "1";
             }
-        }
         return Integer.parseInt(highest, 2) * Integer.parseInt(lowest, 2);
     }
 
@@ -46,19 +39,14 @@ public class day3 {
 
     private static int twoHelper(boolean one) throws FileNotFoundException {
         Scanner s = new Scanner(new File("input.txt"));
-        LinkedList<String> list = new LinkedList<>();
-        LinkedList<String> listOne = new LinkedList<>();
-        LinkedList<String> listTwo = new LinkedList<>();
-        while (s.hasNext()) {
+        LinkedList<String> list = new LinkedList<>(), listOne = new LinkedList<>(), listTwo = new LinkedList<>();
+        while (s.hasNext())
             list.add(s.next());
-        }
         int amountOne = 0, amountTwo = 0;
         for (int j = 0; j < 12; j++) {
-            if (listOne.size() + listTwo.size() == 1) {
+            if (amountOne + amountTwo == 1)
                 continue;
-            }
-            amountOne = 0;
-            amountTwo = 0;
+            amountOne = amountTwo = 0;
             listOne = new LinkedList<>();
             listTwo = new LinkedList<>();
             for (int i = 0; i < list.size(); i++) {
@@ -70,22 +58,9 @@ public class day3 {
                     listTwo.add(list.get(i));
                 }
             }
-            if (one) {
-                if (amountOne >= amountTwo) {
-                    list = listOne;
-                } else {
-                    list = listTwo;
-                }
-            } else {
-
-                if (amountOne < amountTwo && amountOne != 0) {
-                    list = listOne;
-                } else {
-                    list = listTwo;
-                }
-            }
+            list = one ? (amountOne >= amountTwo ? listOne : listTwo)
+                    : (amountOne < amountTwo && amountOne != 0 ? listOne : listTwo);
         }
-
         return amountOne == 1 ? Integer.parseInt(listOne.get(0), 2) : Integer.parseInt(listTwo.get(0), 2);
     }
 }

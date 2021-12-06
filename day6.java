f67609395a82ee98cca03dc0f2ef35cb57a51c62
Bas Buijsen <72604903+deBasMan21@users.noreply.github.com>
2021-12-06 11:13:25 +0100

    Update day6.java

--- day6.java
+++ day6.java
@@ -14,9 +14,8 @@ public class day6 {
         String[] input = new Scanner(new File("input.txt")).nextLine().split(",");
         HashMap<Integer, Long> fish = new HashMap<>();
         for (String string : input) fish.put(Integer.parseInt(string), fish.get(Integer.parseInt(string)) != null ? fish.get(Integer.parseInt(string)) + 1L : 1L);
-        long zero = 0;
         for (int i = 0; i < (one ? 80 : 256); i++) {
-            zero = fish.get(0) != null ? fish.get(0) : 0;
+            long zero = fish.get(0) != null ? fish.get(0) : 0;
             for (int j = 0; j < 9; j++)
                 if (j == 6) fish.put(j, (fish.get(j + 1) != null ? fish.get(j + 1) : 0) + zero);
                 else fish.put(j, j == 8 ? zero : (fish.get(j + 1) != null ? fish.get(j + 1) : 0));

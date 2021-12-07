2b372574270ce945d2a32980bb2dff84a646ae7c
Bas Buijsen <72604903+deBasMan21@users.noreply.github.com>
2021-12-07 10:31:56 +0100

    Update day7.java

--- day7.java
+++ day7.java
@@ -13,16 +13,11 @@ public class day7 {
     public static int exerciseOne(boolean one) throws FileNotFoundException {
         String[] input = new Scanner(new File("input.txt")).nextLine().split(",");
         HashMap<Integer, Integer> crabs = new HashMap<>();
-        for (String string : input)
-            crabs.put(Integer.parseInt(string),
-                    crabs.get(Integer.parseInt(string)) != null ? crabs.get(Integer.parseInt(string)) + 1 : 1);
+        for (String string : input) crabs.put(Integer.parseInt(string), crabs.get(Integer.parseInt(string)) != null ? crabs.get(Integer.parseInt(string)) + 1 : 1);
         int amount = 0;
         for (int i = 1; i < 2000; i++) {
             int fuel = 0;
-            for (Entry<Integer, Integer> crab : crabs.entrySet())
-                fuel += one ? Math.abs(crab.getKey() - i) * crab.getValue()
-                        : Math.abs(crab.getKey() - i) * (Math
-                                .abs(crab.getKey() - i) + 1) / 2 * crab.getValue();
+            for (Entry<Integer, Integer> crab : crabs.entrySet()) fuel += one ? Math.abs(crab.getKey() - i) * crab.getValue() : Math.abs(crab.getKey() - i) * (Math.abs(crab.getKey() - i) + 1) / 2 * crab.getValue();
             amount = fuel < amount || amount == 0 ? fuel : amount;
         }
         return amount;

f6245b30828666f9c38e5aac451746a83a7aec7e
Bas <bbuijsen@gmail.com>
2021-12-08 11:37:07 +0100

    simplified code

--- day8.java
+++ day8.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Map.Entry;
 
@@ -15,15 +16,14 @@ public class day8 {
     public static int exerciseOne(boolean exOne) throws FileNotFoundException {
         Scanner s = new Scanner(new File("input.txt"));
         LinkedList<String> input = new LinkedList<>();
-        int total = 0, sum = 0;
-        String[] segmentLetters = { "a", "b", "c", "d", "e", "f", "g" };
+        int total = 0;
         while (s.hasNextLine()) input.add(s.nextLine());
         for (String strings : input) {
-            char[] segments = { ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-            String[] numbers = strings.split(" | "), answer = { numbers[11], numbers[12], numbers[13], numbers[14] };
+            char[] segments = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+            String[] numbers = strings.split(" | "), answer = { numbers[11], numbers[12], numbers[13], numbers[14] }, options = { "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg" }, segmentLetters = { "a", "b", "c", "d", "e", "f", "g" };
             int[] amounts = {0, 0, 0, 0, 0, 0, 0, 0};
             String twoChar = "", threeChar = "", value = "";
-            HashMap<String, Integer> letters = new HashMap<>();
+            HashMap<String, Integer> letters = new HashMap<>(), values = new HashMap<>(Map.of("8", 2, "4", 4, "9", 5, "6", 1));
             for (int i = 10; i < 15; i++) numbers[i] = "";
             for (String string : numbers)
                 if (string.length() == 2) twoChar = string;
@@ -33,35 +33,24 @@ public class day8 {
             for (String string : numbers)
                 for (int i = 0; i < string.length(); i++) letters.put(string.charAt(i) + "", letters.getOrDefault(string.charAt(i) + "", 0) + 1);
             for (Entry<String, Integer> entry : letters.entrySet())
-                if (entry.getValue() == 6) segments[1] = entry.getKey().charAt(0);
-                else if (entry.getValue() == 8 && entry.getKey().charAt(0) != segments[0]) segments[2] = entry.getKey().charAt(0);
-                else if (entry.getValue() == 4) segments[4] = entry.getKey().charAt(0);
-                else if (entry.getValue() == 9) segments[5] = entry.getKey().charAt(0);
+                segments[values.getOrDefault(entry.getValue().toString(), 7)] = entry.getKey().charAt(0) != segments[0] ? entry.getKey().charAt(0) : segments[values.get(entry.getValue() + "")];
             for (String string : numbers)
                 if (string.length() == 4) segments[3] = string.replace(segments[1], ' ').replace(segments[2], ' ').replace(segments[5], ' ').trim().charAt(0);
             for (String string : numbers)
                 if (string.length() == 7) segments[6] = string.replace(segments[0], ' ').replace(segments[1], ' ').replace(segments[2], ' ').replace(segments[3], ' ').replace(segments[4], ' ').replace(segments[5], ' ').trim().charAt(0);
-            for (String string : answer)
-                amounts[string.length()] += 1;
+            for (String string : answer) amounts[string.length()] += 1;
             for (String string : answer) {
                 String result = "";
                 for (int i = 0; i < string.length(); i++)
                     for (int j = 0; j < 7; j++)
                         if (string.charAt(i) == segments[j]) result += segmentLetters[j];
-                value += decodeString(result) + "";
+                char[] charArray = result.toCharArray();
+                Arrays.sort(charArray);
+                for (int i = 0; i < options.length; i++)
+                    if (new String(charArray).equals(options[i])) value += i + "";
             }
-            total += amounts[2] + amounts[4] + amounts[3] + amounts[7];
-            sum += Integer.parseInt(value);
+            total += exOne ? amounts[2] + amounts[4] + amounts[3] + amounts[7] : Integer.parseInt(value);
         }
-        return exOne ? total : sum;
-    }
-
-    private static int decodeString(String input) {
-        String[] options = { "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg" };
-        char[] charArray = input.toCharArray();
-        Arrays.sort(charArray);
-        for (int i = 0; i < options.length; i++)
-            if (new String(charArray).equals(options[i])) return i;
-        return -1;
+        return total;
     }
 }

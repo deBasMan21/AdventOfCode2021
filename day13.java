10f2ae899daa85a2283265d4f2da710c2d59640b
Bas <bbuijsen@gmail.com>
2021-12-13 18:27:25 +0100

    day 13

--- day13.java
+++ day13.java
@@ -1,61 +1,80 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class day13 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws FileNotFoundException{
         System.out.println(exerciseOne(true));
         System.out.println(exerciseOne(false));
     }
 
-    public static int exerciseOne(boolean one) throws Exception{
-        Scanner s = new Scanner(new File("inputTest.txt"));
-        String[][] grid = new String[15][15];
-        boolean fillingDone = false;
-        LinkedList<String> folds = new LinkedList<>();
-        while(s.hasNextLine()){
-            String[] coordinates = s.nextLine().split(",");
-            if(fillingDone) folds.add(coordinates[0]);
-            if(coordinates[0].equals("")) fillingDone = true;
-            if(!fillingDone) grid[Integer.parseInt(coordinates[0])][Integer.parseInt(coordinates[1])] = "#";
+    public static Object exerciseOne(boolean one) throws FileNotFoundException{
+        Scanner scanner = new Scanner(new File("input.txt"));
+        Set<Dot> dots = new HashSet<>();
+        while (scanner.hasNext()) {
+            String line = scanner.next();
+            if (line.equals("fold")) break;
+            String[] split = line.split(",");
+            dots.add(new Dot().set(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
         }
-        for (String string : folds) {
-            fold(grid, string);
+        LinkedList<int[]> folds = new LinkedList<>();
+        while (scanner.hasNext()) {
+            if (!scanner.next().equals("along")) continue;
+            String inst = scanner.next();
+            folds.add(new int[]{inst.charAt(0) == 'x' ? 0 : 1, Integer.parseInt(inst.substring(2))});
         }
-        return 0;
+        fold(dots, folds.subList(0, 1));
+        if(one) return dots.size();
+        fold(dots, folds.subList(1, folds.size()));
+        int maxX = max(dots, 0), maxY = max(dots, 1);
+        Dot d = new Dot();
+        String returnValue = "";
+        for (int i = 0; i <= maxY; i++) {
+            for (int j = 0; j <= maxX; j++) returnValue += dots.contains(d.set(j, i)) ? '#' : '.';
+            returnValue += "\n";
+        }
+        return returnValue;
     }
 
-    public static void fold(String[][] grid, String fold){
-        String[] foldCoordinates = fold.split(" ");
-        int coordinate = Integer.parseInt(foldCoordinates[2].substring(2));
-        if(foldCoordinates[2].charAt(0) == 'x'){
-            print(grid);
-            String[][] foldPart = new String[15][15];
-            int index = 0;
-            for(int i = grid.length - 1; i > coordinate ; i--){
-                for (int j = 0; j < grid[0].length; j++) {
-                    foldPart[index][j] = grid[i][j];
-                }
-                index++;
-            }
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            print(foldPart);
-        } else {
-            System.out.println(foldCoordinates[2].substring(2));
+    public static void fold(Set<Dot> dots, List<int[]> folds){
+        for (int i = 0; i < folds.size(); i++) {
+            int a = folds.get(i)[0], mid = folds.get(i)[1];
+            dots.stream()
+                .filter(d -> a == 0 ? d.x > mid : d.y > mid)
+                .collect(Collectors.toList())
+                    .stream()
+                    .peek(dots::remove)
+                    .map(d -> a == 0 ? d.set(mid * 2 - d.x, d.y) : d.set(d.x, mid * 2 - d.y))
+                    .forEach(dots::add);
         }
     }
 
-    private static void print(String[][] grid){
-        for (String[] strings : grid) {
-            for (String strings2 : strings) {
-                if(strings2 != null){
-                    System.out.print(strings2 + "  ");
-                } else {
-                    System.out.print(".  ");
-                }
-            }
-            System.out.println();
+    public static int max(Set<Dot> dots, int a){
+        int biggest = 0;
+        for (Dot dot : dots) biggest = a == 0 ? (dot.x > biggest ? dot.x : biggest) : (dot.y > biggest ? dot.y : biggest);
+        return biggest;
+    }
+
+    public static class Dot{
+        public int x, y;
+
+        public Dot set(int x, int y){
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+
+        @Override
+        public boolean equals(Object o){
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Dot dot = (Dot)o;
+            return x == dot.x && y == dot.y;
+        }
+
+        @Override
+        public int hashCode(){
+            return Objects.hash(x, y);
         }
     }
 }
